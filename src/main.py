a832e7b48ad1cc9360c753c970c673774511f94c
Eric Qian <eric.qian@nasa.gov>
2023-07-26 13:47:03 -0400

    Revised

--- src/main.py
+++ src/main.py
@@ -35,7 +35,7 @@ config_data = load_config(config_file)
 
 error_messages = {
     "MEASURE_CAT_ERROR": lambda measure_cat:
-    f"Measurement category ({click.style(measure_cat, fg='red')}) is not valid; valid measurement categories are "
+    f"Measurement category {click.style(measure_cat, fg='red')} is not valid; valid measurement categories are "
     f"{click.style(', '.join([str(s) for s in list(config_data['MeasurementCategory'].keys())]), fg='green')}",
 
     "NUM_ATTRIBUTES_ERROR": lambda num_attributes, expected:
@@ -43,38 +43,45 @@ error_messages = {
     f"number of attributes ({click.style(expected, fg='green')}).",
 
     "ACQUISITION_ERROR": lambda acquisition_met:
-    f"Acquisition method ({click.style(acquisition_met, fg='red')}) is not valid; valid acquisition methods are "
+    f"Acquisition method {click.style(acquisition_met, fg='red')} is not valid; valid acquisition methods are "
     f"{click.style(', '.join([str(s) for s in config_data['AcquisitionMethod']]), fg='green')}",
 
     "CORE_NAME_ERROR": lambda core_name, measure_cat:
-    f"Core Name ({click.style(core_name, fg='red')}) is not valid for the measurement category "
+    f"Core Name {click.style(core_name, fg='red')} is not valid for the measurement category "
     f"{click.style(measure_cat, fg='blue')}.",
 
     "SPECIFICITY_ERROR": lambda specificity, expected:
-    f"Specificity attribute ({click.style(specificity, fg='red')}) does not match with the core name; "
+    f"Specificity attribute {click.style(specificity, fg='red')} does not match with the core name; "
     f"expected to see {click.style(expected, fg='green')}",
 
-    "REPORTING_ERROR": lambda reporting:
-    f"Reporting attribute ({click.style(reporting, fg='red')}) is not valid.",
-
-    "WL_ERROR": lambda wave_length:
-    f"Wave length attribute ({click.style(wave_length, fg='red')}) is not valid.",
-
-    "MEASUREMENT_RH_ERROR": lambda relative_humidity:
-    f"MeasurementRH attribute ({click.style(relative_humidity, fg='red')}) is not valid.",
-
-    "SIZING_TECHNIQUE_ERROR": lambda sizing_technique:
-    f"SizingTechnique attribute ({click.style(sizing_technique, fg='red')}) is not valid.",
-
-    "SIZE_RANGE_ERROR": lambda size_range:
-    f"SizeRange attribute ({click.style(size_range, fg='red')}) is not valid.",
-
-    "MEASURE_DIR_ERROR": lambda measurement_direction, valid_list:
-    f"MeasurementDirection attribute ({click.style(measurement_direction, fg='red')}) is not valid; must be "
+    "REPORTING_ERROR": lambda reporting, valid_list:
+    f"Reporting attribute {click.style(reporting, fg='red')} is not valid; valid attributes are "
     f"{click.style(', '.join([str(s) for s in valid_list]), fg='green')}",
 
+    "WL_ERROR": lambda wave_length, valid_list:
+    f"WaveLength attribute {click.style(wave_length, fg='red')} is not valid; valid attributes are "
+    f"{click.style(', '.join([str(s) for s in valid_list]), fg='green')}",
+
+    "MEASUREMENT_RH_ERROR": lambda relative_humidity, valid_list:
+    f"MeasurementRH attribute {click.style(relative_humidity, fg='red')} is not valid; valid attributes are "
+    f"{click.style(', '.join([str(s) for s in valid_list]), fg='green')}",
+
+    "SIZING_TECHNIQUE_ERROR": lambda sizing_technique, valid_list:
+    f"SizingTechnique attribute {click.style(sizing_technique, fg='red')} is not valid; valid attributes are "
+    f"{click.style(', '.join([str(s) for s in valid_list]), fg='green')}",
+
+    "SIZE_RANGE_ERROR": lambda size_range, sizing_technique, valid_list:
+    f"SizeRange attribute {click.style(size_range, fg='red')} is not valid; valid attributes are "
+    f"{click.style(', '.join([str(s) for s in valid_list]), fg='green')}" if sizing_technique != 'None' else
+    f"SizingRange attribute must be {click.style('Bulk', fg='green')} if SizingTechnique is "
+    f"{click.style('None', fg='blue')}",
+
+    "MEASURE_DIR_ERROR": lambda measurement_direction, valid_list:
+    f"MeasurementDirection attribute {click.style(measurement_direction, fg='red')} is not valid; valid attributes "
+    f"are {click.style(', '.join([str(s) for s in valid_list]), fg='green')}",
+
     "SPECTRAL_COV_ERROR": lambda spectral_coverage, valid_list:
-    f"SpectralCoverage attribute ({click.style(spectral_coverage, fg='red')}) is not valid; must be "
+    f"SpectralCoverage attribute {click.style(spectral_coverage, fg='red')} is not valid; valid attributes are "
     f"{click.style(', '.join([str(s) for s in valid_list]), fg='green')}",
 
     "PRODUCT_ERROR": lambda products, valid_list:
@@ -84,7 +91,7 @@ error_messages = {
     f"{click.style('NoProductsSpecified', fg='green')}",
 
     "WL_MODE_ERROR": lambda wl_mode, valid_list:
-    f"WLMode attribute ({click.style(wl_mode, fg='red')}) is not valid; must be "
+    f"WLMode attribute ({click.style(wl_mode, fg='red')}) is not valid; valid attributes are "
     f"{click.style(', '.join([str(s) for s in valid_list]), fg='green')}",
 
     "ZERO_ATTRIBUTE_ERROR": lambda measure_cat:
@@ -162,7 +169,7 @@ class StandardName:
         if not valid_specificity and valid_core_name:
             self.error_codes.append(error_messages["SPECIFICITY_ERROR"](specificity, expected_specificity))
         if not valid_reporting:
-            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting))
+            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting, gas_vocab["Reporting"]))
 
         return valid_core_name and valid_reporting and valid_specificity
 
@@ -185,13 +192,17 @@ class StandardName:
         if not valid_core_name:
             self.error_codes.append(error_messages["CORE_NAME_ERROR"](core_name, self.measurement_cat))
         if not valid_rh:
-            self.error_codes.append(error_messages["MEASUREMENT_RH_ERROR"](relative_humidity))
+            self.error_codes.append(error_messages["MEASUREMENT_RH_ERROR"](relative_humidity,
+                                                                           aerosol_vocab["MeasurementRH"]))
         if not valid_size_technique:
-            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique))
+            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique,
+                                                                             aerosol_vocab["SizingTechnique"]))
         if not valid_size_range:
-            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range))
+            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range, sizing_technique,
+                                                                       aerosol_vocab["SizeRange"]))
         if not valid_reporting:
-            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting))
+            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting,
+                                                                      aerosol_vocab["AerMP/AerOpt Reporting"]))
 
         return valid_core_name and valid_rh and valid_size_technique and valid_size_range and valid_reporting
 
@@ -212,11 +223,13 @@ class StandardName:
         if not valid_core_name:
             self.error_codes.append(error_messages["CORE_NAME_ERROR"](core_name, self.measurement_cat))
         if not valid_size_technique:
-            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique))
+            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique,
+                                                                             aerosol_vocab["SizingTechnique"]))
         if not valid_size_range:
-            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range))
+            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range, sizing_technique,
+                                                                       aerosol_vocab["SizeRange"]))
         if not valid_reporting:
-            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting))
+            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting, aerosol_vocab["AerComp Reporting"]))
 
         return valid_core_name and valid_size_technique and valid_size_range and valid_reporting
 
@@ -238,13 +251,16 @@ class StandardName:
         if not valid_core_name:
             self.error_codes.append(error_messages["CORE_NAME_ERROR"](core_name, self.measurement_cat))
         if not valid_wl:
-            self.error_codes.append(error_messages["WL_ERROR"](wave_length))
+            self.error_codes.append(error_messages["WL_ERROR"](wave_length, aerosol_vocab["WL"]))
         if not valid_rh:
-            self.error_codes.append(error_messages["MEASUREMENT_RH_ERROR"](relative_humidity))
+            self.error_codes.append(error_messages["MEASUREMENT_RH_ERROR"](relative_humidity,
+                                                                           aerosol_vocab["MeasurementRH"]))
         if not valid_size_range:
-            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range))
+            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range, "Default",
+                                                                       aerosol_vocab["SizeRange"]))
         if not valid_reporting:
-            self.error_codes.append(error_messages["REPORTING_ERROR"](valid_reporting))
+            self.error_codes.append(error_messages["REPORTING_ERROR"](valid_reporting,
+                                                                      aerosol_vocab["AerMP/AerOpt Reporting"]))
 
         return valid_core_name and valid_wl and valid_rh and valid_size_range and valid_reporting
 
@@ -265,11 +281,13 @@ class StandardName:
         if not valid_core_name:
             self.error_codes.append(error_messages["CORE_NAME_ERROR"](core_name, self.measurement_cat))
         if not valid_size_technique:
-            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique))
+            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique,
+                                                                             cloud_vocab["SizingTechnique"]))
         if not valid_size_range:
-            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range))
+            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range, sizing_technique,
+                                                                       cloud_vocab["SizeRange"]))
         if not valid_reporting:
-            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting))
+            self.error_codes.append(error_messages["REPORTING_ERROR"](reporting, cloud_vocab["CldComp Reporting"]))
 
         return valid_core_name and valid_size_technique and valid_size_range and valid_reporting
 
@@ -290,11 +308,14 @@ class StandardName:
         if not valid_core_name:
             self.error_codes.append(error_messages["CORE_NAME_ERROR"](core_name, self.measurement_cat))
         if not valid_size_technique:
-            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique))
+            self.error_codes.append(error_messages["SIZING_TECHNIQUE_ERROR"](sizing_technique,
+                                                                             cloud_vocab["SizingTechnique"]))
         if not valid_size_range:
-            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range))
+            self.error_codes.append(error_messages["SIZE_RANGE_ERROR"](size_range, sizing_technique,
+                                                                       cloud_vocab["SizeRange"]))
         if not valid_reporting:
-            self.error_codes.append(error_messages["REPORTING_ERROR"](valid_reporting))
+            self.error_codes.append(error_messages["REPORTING_ERROR"](valid_reporting,
+                                                                      cloud_vocab["CldMicro Reporting"]))
 
         return valid_core_name and valid_size_technique and valid_size_range and valid_reporting
 
@@ -326,7 +347,7 @@ class StandardName:
         if not valid_core_name:
             self.error_codes.append(error_messages["CORE_NAME_ERROR"](core_name, self.measurement_cat))
         if not valid_wl:
-            self.error_codes.append(error_messages["WL_ERROR"](wave_length))
+            self.error_codes.append(error_messages["WL_ERROR"](wave_length, cloud_vocab["WL"]))
 
         return valid_core_name and valid_wl
 
